7ee97f31ae6c2e83546c27edf95d8416f3eeb046
maxamed <maxamed@gmail.com>
2024-07-08 12:32:11 +0300

    first commit

---
+++ src/test/java/com/Student_Management_System/SmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Student_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsApplicationTests {
+	public static void main(String[] args) {
+		SpringApplication.run(SmsApplication.class, args);
+	}
+
+}
